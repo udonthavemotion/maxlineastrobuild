7e4055838e324938b7ca45c6653289fc8bde9041
udonthavemotion <zeromotionprofits@outlook.com>
2025-07-15 23:45:59 -0500

    Add Vercel deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://maxline-bulldogs.vercel.app",
   integrations: [mdx(), sitemap(), icon()],
+  output: "server",
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
